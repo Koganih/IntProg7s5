7abcd2394315055977ff369868b353ec6cb5ea62
Koganih <gizamora@uamv.edu.ni>
2025-04-23 13:44:11 -0600

    edad

---
+++ Edad_Ejercicio.py
@@ -0,0 +1,5 @@
+edad = int(input("Ingrese su edad: "))
+if edad < 18:
+    print("Usted no puede votar")
+elif edad >= 18:
+    print("Usted puede votar")
--- Velocidad.py
+++ Velocidad.py
@@ -1,3 +1,5 @@
 velocidad = int(input("Ingrese la velocidad: "))
 if velocidad > 120:
     print("Exceso de velocidad ¡reduzca la velocidad!")
+else:
+    print("Velocidad dentro de los limites permitidos")
